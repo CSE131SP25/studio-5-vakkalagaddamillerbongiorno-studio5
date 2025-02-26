c80fe143b02f3e957850f17f8e41a868be961218
praneelvakkalagadda <praneelvakkalagadda@localhost>
2025-02-26 14:59:16 -0600

    Studio 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,7 +14,8 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		double distance = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+		
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
 		
 		return distance;
